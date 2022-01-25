2fbbf055d5216bf09c68c784a9165163b03a2cbb
Anagh Kanungo <23740251+anaghkanungo7@users.noreply.github.com>
2022-01-25 07:22:15 -0500

    Version 2.0
    - Added slider
    - Added Calculate button
    - Moved around a few things

--- iOS Tip Calculator/iOS Tip Calculator/ViewController.swift
+++ iOS Tip Calculator/iOS Tip Calculator/ViewController.swift
@@ -17,21 +17,54 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var totalLabel: UILabel!
     
+    @IBOutlet weak var percentageSlider: UISlider!
+    
+    @IBOutlet weak var percentageLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
 
+//    @IBAction func calculateTip(_ sender: Any) {
+//        //        Fetch bill amount
+//        let bill = Double(billAmountTextField.text!) ?? 0
+//
+//        //        Get total tip
+//        let tipPercentages = [0.15, 0.18, 0.2, 0.25]
+//        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+//        let total = bill + tip
+//
+//        //        Update labels
+//        tipAmountLabel.text = String(format: "$%.2f", tip)
+//        totalLabel.text = String(format: "$%.2f", total)
+//    }
+    
+    @IBAction func updatePercentageFromControl(_ sender: Any) {
+        let tipPercentages = [15, 18, 20, 25]
+        let perc = Int( tipPercentages[tipControl.selectedSegmentIndex])
+        
+        //        Update label
+        percentageLabel.text = String(format: "%u", perc)
+        //        Keep slider in sync
+        percentageSlider.value = Float(perc)
+    }
+    
+    @IBAction func updatePercentageFromSlider(_ sender: Any) {
+        let perc = Int(percentageSlider.value)
+        percentageLabel.text = String(format: "%u", perc)
+    }
+    
     @IBAction func calculateTip(_ sender: Any) {
         //        Fetch bill amount
         let bill = Double(billAmountTextField.text!) ?? 0
-        
+
         //        Get total tip
-        let tipPercentages = [0.15, 0.18, 0.2]
-        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let tipPercentage = Double(percentageLabel.text ?? "") ?? 0.0
+        print(tipPercentage)
+        let tip = bill * (tipPercentage / 100)
         let total = bill + tip
-        
+
         //        Update labels
         tipAmountLabel.text = String(format: "$%.2f", tip)
         totalLabel.text = String(format: "$%.2f", total)
